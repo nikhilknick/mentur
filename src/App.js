0a86bfd9ed4a41cc87058c2c53ece77a0a45d87d
Nikhil Arya <nikhilknick@gmail.com>
2020-12-27 14:49:46 +0530

    Change commas

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import image from './comingsoon.png'
-import menturlogo from './mento.png';
+import React from "react";
+import logo from "./logo.svg";
+import image from "./comingsoon.png";
+import menturlogo from "./mento.png";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <img className="heading-1" src={menturlogo}/>
-    <div  className="comingsoon">
-      <img src={image} />
-    </div>
+      <img className="heading-1" src={menturlogo} />
+      <div className="comingsoon">
+        <img src={image} />
+      </div>
     </div>
   );
 }
